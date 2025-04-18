d5232947518cd930bb527d8240164a253bb220df
ThomasTramarin <thomas.tramarin.dev@gmail.com>
2025-04-18 14:35:02 +0200

    feat: error handler and custom ApiError

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import asyncHandler from "express-async-handler";
+import { errorHandler } from "./middlewares/errorHandler";
+import { ApiError } from "./utils/ApiError";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    msg: "Test message!",
-  });
-});
+app.use(express.json());
+
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response) => {
+    throw new ApiError("Something went wrong", 500);
+  })
+);
+
+app.use(errorHandler);
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
